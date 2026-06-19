af0d92072bc5dbb899e059e3348e5d03e221c8b5
Chakkor Bakr <bakr.chakk@gmail.com>
2026-06-19 23:17:12 +0200

    Add basic http server

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
-
+#include <WiFi.h>
+#include <WebServer.h>
 // put function declarations here:
-int printText(const char*);
+void setupServer();
 HardwareSerial printer(2);
 
+WebServer server(80);
+
 void setup() {
+  setupServer();    
+
   // put your setup code here, to run once:
   delay(2000);   // critical
   printer.begin(
@@ -18,7 +23,7 @@ void setup() {
   delay(50);
   printer.println("******************************");
   delay(50);
-  printer.println("********* HELLO BABY *********");
+  printer.println("********* HELLO GOAT *********");
   delay(50);
   printer.println("******************************");
   delay(50);
@@ -28,9 +33,39 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  server.handleClient();
 }
 
-// put function definitions here:
-int printText(const char* str) {
-  printer.println(str);
+void setupServer() {
+    const char* ssid = "glinet";
+    const char* password = "aJpxasPX13haHeYwam";
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+
+    server.on("/test", HTTP_GET, []() {
+        server.send(200, "text/plain", "ok");
+    });
+    server.on(
+    "/print/raw",
+    HTTP_POST,
+    []()
+    {
+      String body = server.arg("plain");
+
+      printer.write(
+          (const uint8_t*)body.c_str(),
+          body.length()
+      );
+
+      server.send(
+          200,
+          "application/json",
+          "{\"ok\":true}"
+      );
+    }
+  );
+    server.begin();
 }
